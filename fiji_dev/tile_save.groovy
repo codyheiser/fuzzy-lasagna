ad6645a109a41bf2c5eb5096ef15686cd84add56
Cody Heiser <codyheiser49@gmail.com>
2019-04-04 15:01:12 -0500

    example tiles for prototyping with opencv-python

--- fiji_dev/tile_save.groovy
+++ fiji_dev/tile_save.groovy
@@ -4,6 +4,7 @@
 
 import ij.IJ
 import ij.ImagePlus
+import ij.WindowManager
 import qupath.imagej.gui.IJExtension
 import qupath.imagej.helpers.IJTools
 import qupath.imagej.images.servers.ImagePlusServer
@@ -58,7 +59,7 @@ double tileHeight = tileHeightPixels * downsample
 // loop through the image - including z-slices (even though there's normally only one...)
 int counter = 0;
 for (int z = 0; z < server.nZSlices(); z++) { 
-    for (double y = 0; y < 1; y += tileHeight) { // replace number with server.getHeight()
+    for (double y = 0; y < server.getHeight(); y += tileHeight) { // replace number with server.getHeight()
 
         // compute integer y coordinates
         int yi = (int)(y + 0.5)
@@ -71,7 +72,7 @@ for (int z = 0; z < server.nZSlices(); z++) {
             continue
         }
 
-        for (double x = 0; x < 1; x += tileWidth) { // replace number with server.getWidth()
+        for (double x = 0; x < server.getWidth(); x += tileWidth) { // replace number with server.getWidth()
 
             // compute integer x coordinates
             int xi = (int)(x + 0.5)
@@ -89,51 +90,34 @@ for (int z = 0; z < server.nZSlices(); z++) {
                 continue
             }
 
-            // surround with try/catch in case the server gives us trouble
-            try {
-                // read the image region
-                println("Sending tile " + counter + " to ImageJ")
-                ImagePlus imp = server.readImagePlusRegion(request).getImage(false)
-                // show the image
-                imp.show()
+            // read the image region
+            println("Sending tile " + counter + " to ImageJ")
+            ImagePlus imp = server.readImagePlusRegion(request).getImage(false)
+            // show the image
+            imp.show()
+            // process 3-channel image to make it pretty
+            imp = IJ.getImage();
+            // get composite of three channels
+            IJ.run(imp, "Stack to RGB", "");
+            names = WindowManager.getImageTitles()
+            imp1 = WindowManager.getImage(names[1]); 
+            // adjust brightness and contrast to make pretty export image
+            IJ.run("Brightness/Contrast...");
+            IJ.setMinAndMax(imp1, 0, 100);
 
-                // process 3-channel image to make it pretty for ilastik
-                imp = IJ.getImage();
-
-                // get composite of three channels
-                IJ.run("Make Composite", "display=Composite");
-
-                // adjust brightness and contrast to make pretty export image
-                IJ.run("Brightness/Contrast...");
-                IJ.setMinAndMax(imp, 0, 65000);
-                IJ.run(imp, "Next Slice [>]", "");
-                IJ.setMinAndMax(imp, 0, 13000);
-                IJ.run(imp, "Next Slice [>]", "");
-                IJ.setMinAndMax(imp, 0, 12000);
-                IJ.run(imp, "Apply LUT", "");
-
-                // get a suitable file name
-                String name = String.format("%s (d=%.2f, x=%d, y=%d, w=%d, h=%d, z=%d).%s", serverName, downsample, xi, yi, wi, hi, z, ext)
-                
-                // create an output file
-                File file = new File(dirOutput, name)
-                
-                // save the image
-                IJ.save(imp, file.getAbsolutePath())
-                
-                // print progress
-                counter++
-                println("Written tile " + counter + " to " + file.getAbsolutePath())
-
-            } catch (Exception e) {
-                // check if we have had a sufficient number of errors to just give up
-                nErrors++;
-                if (nErrors > maxErrors) {
-                    println("Maximum number of errors exceeded - aborting...")
-                    return
-                }
-                e.printStackTrace()
-            }
+            // get a suitable file name
+            String name = String.format("%s (d=%.2f, x=%d, y=%d, w=%d, h=%d, z=%d).%s", serverName, downsample, xi, yi, wi, hi, z, ext)
+            
+            // create an output file
+            File file = new File(dirOutput, name)
+            
+            // save the image
+            IJ.save(imp1, file.getAbsolutePath())
+            
+            // print progress
+            println("Written tile " + counter + " to " + file.getAbsolutePath())
+            counter++
+            IJ.run("Close All", "");
         }
     }
 }
